42c1d2b4f6371d311b0075e895a3a63d9b91fc0c
hiisox <mehdmhaya@gmail.com>
2023-08-11 17:13:15 +0200

    k

--- philo/srcs/philo.c
+++ philo/srcs/philo.c
@@ -21,36 +21,41 @@ long int	gettodaystime(void)
 	return ((current.tv_sec * 1000) + (current.tv_usec / 1000));
 }
 
+int sleeper(t_philo *philo, long duration, long start)
+{
+	int current;
+	
+	while (1)
+	{
+		current = gettodaystime() - start;
+		if (current >= duration)
+			break ;
+		if (duration - current > 1000)
+			usleep(100);
+		else
+			usleep((duration - current) / 10);
+		pthread_mutex_lock(&philo->data->deathchecker);
+		if (philo->data->death == 0)
+		{
+			pthread_mutex_unlock(&philo->data->deathchecker);
+			return (1);
+		}
+		pthread_mutex_unlock(&philo->data->deathchecker);
+	}
+	return (0);
+}
+
 int	my_usleep(long int duration, t_philo *philo)
 {
 	long int	start;
-	long int	current;
 
 	(void)philo;
 	start = -1;
 	start = gettodaystime();
 	if (start == -1)
 		return (1);
-	else
-	{
-		while (1)
-		{
-			current = gettodaystime() - start;
-			if (current >= duration)
-				break ;
-			if (duration - current > 1000)
-				usleep(100);
-			else
-				usleep((duration - current) / 10);
-			pthread_mutex_lock(&philo->data->deathchecker);
-			if (philo->data->death == 0)
-			{
-				pthread_mutex_unlock(&philo->data->deathchecker);
-				return (1);
-			}
-			pthread_mutex_unlock(&philo->data->deathchecker);
-		}
-	}
+	else if (sleeper(philo, duration, start))
+		return (1);
 	return (0);
 }
 
@@ -83,6 +88,7 @@ void	init(t_data *data, char **av, int ac)
 
 int	lets_malloc(t_data *data, t_philo *philo)
 {
+
 	data->phils = malloc(sizeof(pthread_t) * data->nb_philo);
 	if (!data->phils)
 		return (free(philo), 1);
@@ -97,11 +103,66 @@ int	lets_malloc(t_data *data, t_philo *philo)
 	return (0);
 }
 
+void init_philos(t_data *data, t_philo *philo){
+	int	i;
+	
+	i = 0;
+	while (i < data->nb_philo)
+	{
+		philo[i].id = i + 1;
+		philo[i].eaten = 0;
+		philo[i].data = data;
+		pthread_mutex_init(&philo[i].lunchchecker, NULL);
+		philo[i].l_spoon = &(data->spoon[i]);
+		if (philo[i].id == data->nb_philo)
+			philo[i].r_spoon = &(data->spoon[0]);
+		else
+			philo[i].r_spoon = &(data->spoon[i + 1]);
+		i++;
+	}
+
+}
+
+void init_mutex(t_data *data)
+{
+	int i;
+	
+	i = 0;
+	while (i < data->nb_philo)
+		pthread_mutex_init(&data->spoon[i++], NULL);
+	pthread_mutex_init(&data->print, NULL);
+	pthread_mutex_init(&data->deathchecker, NULL);
+	pthread_mutex_init(&data->eatchecker, NULL);
+}
+
+int creat_thread(t_data *data, t_philo *philo)
+{
+	int	i;
+
+	i = 0;
+	while (i < data->nb_philo)
+	{
+		if (pthread_create(&data->phils[i], NULL, &process_func, &philo[i]))
+			return (perror("CREATION OF THREAD FAILED\n"), 1);
+		i++;
+	}
+	i = 0;
+	pthread_create(&data->watcher, NULL, &check_time_pass, data);
+	while (i < data->nb_philo)
+	{
+		if (pthread_join(data->phils[i], NULL))
+			return (perror("CREATION OF THREAD FAILED\n"), 1);
+		i++;
+	}
+	if (pthread_join(data->watcher, NULL))
+		return (perror("CREATION OF THREAD FAILED\n"), 1);
+	return (0);
+}
+
 int	main(int ac, char **av)
 {
 	static t_data	data = {0};
 	t_philo			*philo;
-	int				i;
 
 	if (ac < 5 || ac > 6)
 		return (printf("NOT VALID AMOUNT OF ARGUMENTS\n"), 1);
@@ -114,53 +175,11 @@ int	main(int ac, char **av)
 	if (lets_malloc(&data, philo))
 		return (1);
 	init(&data, av, ac);
-	i = 0;
-	while (i < data.nb_philo)
-		pthread_mutex_init(&data.spoon[i++], NULL);
-	pthread_mutex_init(&data.print, NULL);
-	pthread_mutex_init(&data.eatchecker, NULL);
-	pthread_mutex_init(&data.deathchecker, NULL);
-	i = 0;
-	while (i < data.nb_philo)
-	{
-		philo[i].id = i + 1;
-		philo[i].eaten = 0;
-		philo[i].data = &data;
-		pthread_mutex_init(&philo[i].lunchchecker, NULL);
-		philo[i].l_spoon = &(data.spoon[i]);
-		if (philo[i].id == data.nb_philo)
-			philo[i].r_spoon = &(data.spoon[0]);
-		else
-			philo[i].r_spoon = &(data.spoon[i + 1]);
-		i++;
-	}
+	init_mutex(&data);
+	init_philos(&data, philo);
 	if (data.nb_philo == 1)
-	{
-		just_one_philo(philo);
-		return (1);
-	}
-	i = 0;
-	while (i < data.nb_philo)
-	{
-		if (pthread_create(&data.phils[i], NULL, &process_func, &philo[i]))
-		{
-			perror("CREATION OF THREAD FAILED\n");
-			return (1);
-		}
-		i++;
-	}
-	i = 0;
-	pthread_create(&data.watcher, NULL, &check_time_pass, &data);
-	while (i < data.nb_philo)
-	{
-		if (pthread_join(data.phils[i], NULL))
-		{
-			perror("CREATION OF THREAD FAILED\n");
-			return (1);
-		}
-		i++;
-	}
-	pthread_join(data.watcher, NULL);
+		return (just_one_philo(philo), 1);
+	creat_thread(&data, philo);
 	free(data.philo);
 	free(data.spoon);
 	free(data.phils);
